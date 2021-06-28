2ea2c4808eb47d536e479a5fc4b31435dd8c2879
Joel Connell <joel.connell@yahoo.com>
2021-06-28 16:25:10 -0700

    Added Skeleton for the form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './css/App.css';
 import React from 'react';
-
+import CharacterForm from './classes/CharacterForm';
 class App extends React.Component {
 
-  render () {
+  render() {
     return (
-      <h1>Welcome To Character Sonnet Front End</h1>
+      <>
+        <h1>Welcome To Character Sonnet Front End</h1>
+
+        <CharacterForm />
+      </>
     );
   }
 }
-    
+
 export default App;
--- src/classes/CharacterForm.js
+++ src/classes/CharacterForm.js
@@ -1,12 +1,71 @@
 import React from 'react';
-
+import Form from 'react-bootstrap/Form'
+// import Button from 'react-bootstrap/buttons';
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import Popover from 'react-bootstrap/Popover';
+// import Header from './Header.js';
+// import Footer from './Footer.js';
 class CharacterForm extends React.Component {
 
-  render () {
+  render() {
     return (
-      <h1>CharacterForm</h1>
+      <>
+      {//This is the start of the Character Creation Form With many modifiers.
+      }
+        <Form>
+          <Form.Group controlID="characterName">
+            <Form.Control type="text" placeholder="Enter your character's name" />
+          </Form.Group>
+
+          <Form.Group controlId="characterClass">
+            <Form.Label>Character Class</Form.Label>
+            <Form.Control as="select" defaultValue="Choose your class!">
+              <option>Class1</option>
+              <option>Class2</option>
+              <option>Class3</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="characterClass">
+            <Form.Label>Character Race</Form.Label>
+            <Form.Control as="select" defaultValue="Choose your race!">
+              <option>Race1</option>
+              <option>Race2</option>
+              <option>Race3</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="characterInformation">
+            <Form.Label></Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Describe your character." />
+          </Form.Group>
+
+        
+
+          <Form.Group controlId="background">
+            <Form.Label>Character Background</Form.Label>
+            <Form.Control as="select" defaultValue="Choose you background">
+              <option>Background</option>
+              <option>Background</option>
+              <option>Background</option>
+              <option>Background</option>
+            </Form.Control>
+          </Form.Group>
+
+          <Form.Group controlId="background">
+            <Form.Label>Character Feature</Form.Label>
+            <Form.Control as="select" defaultValue="Choose you Feature">
+              <option>Feature</option>
+              <option>Feature</option>
+              <option>Feature</option>
+              <option>Feature</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+
+      </>
     );
   }
 }
-    
+
 export default CharacterForm;
