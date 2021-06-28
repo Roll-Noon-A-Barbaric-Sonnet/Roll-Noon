8e27aee60c4230e4f0b8dd15b07ebc07b1e42eb5
Benjamin Ibarra <loco.queens@gmail.com>
2021-06-28 14:52:00 -0700

    pushing for pair coding

---
+++ src/classes/CharacterCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CharacterCard extends React.Component {
+
+  render () {
+    return (
+      <h1>CharacterCard</h1>
+    );
+  }
+}
+    
+export default CharacterCard;
---
+++ src/classes/CharacterMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CharacterMenu extends React.Component {
+
+  render () {
+    return (
+      <h1>CharacterMenu</h1>
+    );
+  }
+}
+    
+export default CharacterMenu;
