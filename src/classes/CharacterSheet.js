b92d9ec35bed3a714b64dc167fe65d4dbbf69211
Benjamin Ibarra <loco.queens@gmail.com>
2021-07-02 07:37:16 -0700

    pushing for handoff

--- src/classes/CharacterSheet.js
+++ src/classes/CharacterSheet.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import CharacterDetails from './CharacterDetails';
 import '../css/CharacterMenu.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
+import ListGroup from 'react-bootstrap/ListGroup';
+import axios from 'axios';
+import CardColumns from 'react-bootstrap/CardColumns'
+
 
 class CharacterSheet extends React.Component {
   constructor(props) {
@@ -16,64 +15,147 @@ class CharacterSheet extends React.Component {
       charData: []
     }
   }
-  async componentDidMount(){
-    let charObj = await this.props.getChar(this.props.mongid);
-    this.setState({charData: charObj.data});
-    console.log('sheet got:',charObj);
+
+
+
+  async componentDidMount() {
+    const config = await this.props.config();
+    console.log('config:', config);
+    const oneChar = await axios.get(`${process.env.REACT_APP_SERVER}/characters/${this.props.mongid}`, config);
+    console.log('that char:', oneChar.data);
+    this.setState({ charData: oneChar.data.character });
   }
 
   render() {
+    console.log('sheetData:', this.state.charData);
     return (
-      <Router>
-        <div id='cardContainer'>
-          <Card id="nameCard">
-            <Card.Title>
-              <div id="cardtitle">
-                {/* <h3 id="h3CharName">{this.data.name}</h3> */}
-              </div>
-            </Card.Title>
-            <Card.Body id="nameCardBody">
-              <div id='cardbody1'>
-                {/* <h6>{this.data.race}</h6>
-                <h6>{this.data.class}</h6> */}
-              </div>
+      this.state.charData.name ?
+        <>
+          <Card style={{ width: '18rem' }}>
+            <Card.Body className='topCard'>
+              <Card.Title>{this.state.charData.name}</Card.Title>
+              <Card.Text>Level: {this.state.charData.level} | {this.state.charData.race} | {this.state.charData.class}</Card.Text>
+
+              <Card.Text>HP: {this.state.charData.hitPoints}</Card.Text>
+              <Card.Text>AC: {this.state.charData.armorClass} Speed: {this.state.charData.speed}</Card.Text>
+            </Card.Body>
+            <Card.Body></Card.Body>
+          </Card>
+          <Card>
+            <Card.Title>Ability Scores</Card.Title>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th class='asTitle'>Stat</th>
+                  <th class='asTitle'>Str</th>
+                  <th class='asTitle'>Dex</th>
+                  <th class='asTitle'>Con</th>
+                  <th class='asTitle'>Int</th>
+                  <th class='asTitle'>Wis</th>
+                  <th class='asTitle'>Cha</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class='tableTitle'>Score</td>
+                  <td class='as'>{this.state.charData.statBlock[0].str}</td>
+                  <td class='as'>{this.state.charData.statBlock[0].dex}</td>
+                  <td class='as'>{this.state.charData.statBlock[0].con}</td>
+                  <td class='as'>{this.state.charData.statBlock[0].int}</td>
+                  <td class='as'>{this.state.charData.statBlock[0].wis}</td>
+                  <td class='as'>{this.state.charData.statBlock[0].cha}</td>
+                </tr>
+                <tr>
+                  <td class='tableTitle'>Mod</td>
+                  <td class='as'>{this.state.charData.statBlock[1].str}</td>
+                  <td class='as'>{this.state.charData.statBlock[1].dex}</td>
+                  <td class='as'>{this.state.charData.statBlock[1].con}</td>
+                  <td class='as'>{this.state.charData.statBlock[1].int}</td>
+                  <td class='as'>{this.state.charData.statBlock[1].wis}</td>
+                  <td class='as'>{this.state.charData.statBlock[1].cha}</td>
+                </tr>
+                <tr>
+                  <td class='tableTitle'>Save</td>
+                  <td class='as'>{this.state.charData.statBlock[2].str}</td>
+                  <td class='as'>{this.state.charData.statBlock[2].dex}</td>
+                  <td class='as'>{this.state.charData.statBlock[2].con}</td>
+                  <td class='as'>{this.state.charData.statBlock[2].int}</td>
+                  <td class='as'>{this.state.charData.statBlock[2].wis}</td>
+                  <td class='as'>{this.state.charData.statBlock[2].cha}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Card>
+          <Card>
+            <Card.Title>Skills</Card.Title>
+            <ListGroup horizontal>
+              {
+                this.state.charData.skills.map(skill =>
+                  <ListGroup.Item className='skills'>{skill.name}</ListGroup.Item>
+                )
+              }
+            </ListGroup>
+          </Card>
+          {
+            this.state.charData.spellcasting ?
+            <Card>
+            <Card.Body>
+              <Card.Text className='spellCasting'>{this.state.charData.spellcasting.spellcasting_ability}</Card.Text>
+
+              <Table>
+              <thead>
+                <tr>
+                  <th class=''></th>
+                </tr>
+              </thead>
+                <tr>
+                  <td class='tableTitle'>Score</td>
+                  <td class='as'>{this.state.charData.statBlock[0].str}</td>
+                </tr>
+              </Table>
+            </Card.Body>
+          </Card> : <></>
+          }
+          <CardColumns>
+            <Card>
+              <Card.Title>Proficiencies</Card.Title>
+            </Card>
+            {
+              this.state.charData.proficiencies.map(prof =>
+                <Card>
+                  <Card.Body>
+                    <Card.Text className='profs'>{prof}</Card.Text>
+                  </Card.Body>
+                </Card>
+              )
+            }
+            {
+              this.state.charData.languages.map(lang =>
+                <Card>
+                  <Card.Body>
+                    <Card.Text className='profs'>{lang}</Card.Text>
+                  </Card.Body>
+                </Card>
+              )
+            }
+          </CardColumns>
+          <Card className='equipment'>
+            <Card.Body>
+              <Card.Title>Equipment</Card.Title>
+              {
+                this.state.charData.equipment.map(equip =>
+                  <Card.Text>{`${equip.equipment.name} ${equip.quantity}`}</Card.Text>
+                )
+              }
             </Card.Body>
           </Card>
-
-          <Card id="hpCard">
-            <Card.Title>
-              <div id="hpCardTitle">
-                {/* <h3 class="hitPoints">{this.data.hitPoints}</h3> */}
-              </div>
-            </Card.Title>
-            <Card.Body class="hpCardBody">
-              <span class="armorSpeed">
-                {/* {this.data.armor} */}
-              </span>
-              <span class="armorSpeed">
-                {/* {this.data.speed} */}
-              </span>
+          <Card>
+            <Card.Body>
+              <Card.Text className='description'>{this.state.charData.description}</Card.Text>
             </Card.Body>
           </Card>
-        </div>
-        <div id="linkContainer">
-          <Link id='panelLink1' to="/onePanel" >Panel 1</Link>
-          <Link id='panelLink2' to="/twoPanel" >Panel 2</Link>
-        </div>
-        <Switch>
-          <Route path='/onePanel'>
-            <CharacterDetails />
-          </Route>
-          <Route path='/twoPanel'>
-            <div id="TwoTwoPanel">
-              <CharacterDetails />
-              <CharacterDetails />
-            </div>
-          </Route>
-
-        </Switch>
-      </Router>
-
+        </>
+        : <></>
     );
   }
 }
